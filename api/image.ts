3a0131a99f49820bc56b50898a5d97a61605e510
upeartaker <upeartaker0@gmail.com>
2024-06-18 01:44:51 +0800

    test

--- api/image.ts
+++ api/image.ts
@@ -1,5 +1,5 @@
 import { Aetolia1Service } from "./generate.js"
-
+export const runtime = 'edge';
 export async function  GET(req:Request){
   const service = new Aetolia1Service()
   const fd = req.url.split('/')
@@ -7,5 +7,5 @@ export async function  GET(req:Request){
 
   const buffer = await service.generateGif(direction)
   req.headers.set('Content-Type', 'image/gif')
-  return new Response(buffer)
+  return new Response('123')
 }
